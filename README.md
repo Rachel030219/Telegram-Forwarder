8dba0869919919638b4dbb9d8417c8a180e0428c
Rachel030219 <291054446@qq.com>
2017-07-19 20:28:32 +0800

    WTF is GPL v2?

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Telegram-Forwarder
+# Telegram Forwarder
 Forward a message or messages in Telegram by bot.
+
+[MAT 语录娘](https://telegram.me/matpin_bot)
+
+### How to use
+
+#### 0. Install pyTelegramBotAPI
+
+Go to [Getting started](https://github.com/eternnoir/pyTelegramBotAPI#getting-started)
+
+#### 1. Change all things you want to
+
+Like tags and IDs.
+
+#### 2. RUN
+
+```shell
+python forwarder.py
+```
+
+#### 3. That's all
---
+++ forwarder.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+import telebot
+
+bot = telebot.TeleBot("<TOKEN>")
+forwarder_list = [235274404, 136971973, 290855719, 242382379, 311300407, 363246572, 184292188]
+
+
+@bot.message_handler(func=lambda message: "#matpin" in message.text)
+def forward_to_matpin(message):
+    if "group" in message.chat.type and str(message.chat.id) == "-1001076357496":
+        send_group_forward(message)
+
+
+@bot.message_handler(commands=['ping'])
+def send_ping(message):
+    bot.send_message(message.chat.id, "群组 id 为 " + str(message.chat.id) + " ，用户 id 为 " + str(message.from_user.id) + " ，完毕。")
+
+
+def send_group_forward(message):
+    if message.from_user.id in forwarder_list:
+        if message.reply_to_message is not None:
+            bot.forward_message("@matpin", message.chat.id, message.reply_to_message.message_id)
+            bot.send_message(message.chat.id, "已经转发过去了哟～")
+        else:
+            bot.reply_to(message, "咱暂时只能转发回复的消息哟～")
+    else:
+        bot.reply_to(message, "你的 PY 度还不够高呢，不能转发消息哟～")
+
+bot.polling()
